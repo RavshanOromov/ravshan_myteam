33bc25e1e6329cdc254f52e5889d692962e74ed3
Ravshan Oromov <ravshanunique@gmail.com>
2022-02-07 22:55:28 +0900

    first commit

---
+++ js/assignments.js
@@ -0,0 +1,5 @@
+let country = 'UZBEKISTAN';
+let continent = 'asia';
+let population = 35;
+console.log('country');
+console.log('continent');
---
+++ js/main.js
@@ -0,0 +1,7 @@
+// S( '.menu__btn').on('click', function(e)
+// {e.preventDefault;
+// S(this).toggleClass('menu-btn__active')});
+const toggleClass = document.querySelector('menu-btn__active');
+const elMenubtn = document.querySelector('.menu__btn');
+elMenubtn.addEventListener('click', function ()
+{elMenubtn.toggleClass.add('menu-btn__active')});
